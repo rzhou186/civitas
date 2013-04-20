576a57752f4a2d74fbb95d175caf814e14acb6ca
gshubham <shubhamgoel123@gmail.com>
2013-04-20 14:09:54 -0700

    Testing XML parser

--- javascripts/companies.js
+++ javascripts/companies.js
@@ -22,7 +22,8 @@ function allJobsFromID(data) {
 
 /* All companies returned . This function adds the data to a list/some structure */
 function allCompanies(data) {
-	var obj = jQuery.parseJSON(data);
+	console.log(data);
+	/*var obj = jQuery.parseJSON(data);
 	for (var i = 0; i < obj.length; i++) {
 		var company = {
 			id : obj[i][0].company_id,
@@ -33,7 +34,7 @@ function allCompanies(data) {
 		};
 		companyData.push(company);
 	}
-	console.log(data);
+	console.log(data);*/
 }
 
 /* make AJAX request to get company data from a given ID */
@@ -93,7 +94,10 @@ function getAllJobs(companyID) {
 function getAllCompanies() {
 
 	var request = $.ajax({
-		url: "http://goodjobs.rogr.me/api/list-companies.php",
+		url : "https://data.guidestar.org/v1/search/?q=zip:10027",
+		username : "bbrandt@guidestar.org",
+		password : "gu1d3st@r"
+		// url: "http://goodjobs.rogr.me/api/list-companies.php",
 		type: "GET",
 		success: function(data){
 			allCompanies(data);
