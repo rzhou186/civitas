fa122a2c16f82f0830b42f5dda99561d7402bb99
Ray Zhou <rzhou186@stanford.edu>
2013-04-20 12:21:41 -0700

    Added companies.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,112 +5,24 @@
  * 
  */
 
-$("#company").click(function(){
-	
- });
+$(document).ready(function(){
 
- /* Company data returned. This function adds the data to a list/some structure */
+	// This variable tracks the current page of the client
+	// 0 = landing page; 1 = selection page; 2 = application page
+	// We might want to stash this variable in cookies later
+	var currStep = 0;
 
- function companyEvent(data) {
- 	console.log(data);
- }
-
-  /* Job data returned. This function adds the data to a list/some structure */
- function jobEvent(data) {
- 	console.log(data);
- }
-
- /* All jobs for a given company returned. This function adds the data to a list/some structure */
- function allJobsFromID(data) {
- 	console.log(data);
- }
-
- /* All companies returned . This function adds the data to a list/some structure */
- function allCompanies(data) {
- 	console.log(data);
- }
-
- /* make AJAX request to get company data from a given ID */
- function getCompanyById(companyID){
-
-	var form_data = {
-		id : companyID
-	};
-
-	var request = $.ajax({
-		url: "http://goodjobs.rogr.me/api/company.php",
-		type: "GET",
-		data: form_data,
-		success: function(data){
-			companyEvent(data);
-		}
+	// Enable next-step buttons
+	$('.next-app-step').click(function(){
+		currStep++;
+		nextAppStep(currStep);
 	});
 
-}
+});
 
- /* make AJAX request to get job data from a given ID */
- function getJobById(jobID){
+/* This function advances the client to the next phase of his/her application */
+function nextAppStep(currStep){
 
-	var form_data = {
-		id : jobID
-	};
-
-	var request = $.ajax({
-		url: "http://goodjobs.rogr.me/api/job.php",
-		type: "GET",
-		data: form_data,
-		success: function(data){
-			jobEvent(data);
-		}
-	});
+	if (currStep === )
 
 }
-
- /* get all job data for a given company */
-function getAllJobs(companyID) {
-
-	var form_data = {
-		id : companyID
-	};
-	var request = $.ajax({
-		url: "http://goodjobs.rogr.me/api/jobs.php",
-		type: "GET",
-		data: form_data,
-		success: function(data){
-			allJobsFromID(data);
-		}
-	});
-
-}
-
- /* make AJAX request to get data for all companies */
-function getAllCompanies() {
-
-		var request = $.ajax({
-		url: "http://goodjobs.rogr.me/api/list-companies.php",
-		type: "GET",
-		success: function(data){
-			allCompanies(data);
-		}
-	});
-}
-
- /* adds the given company (name, description, url) to the database */
-function addCompany(company_name, company_description, company_url) {
-
-		var form_data  = {
-			company_name : company_name,
-			company_description : company_description,
-			company_url : company_url
-		};
-
-		var request = $.ajax({
-		url: "http://goodjobs.rogr.me/api/add-company.php",
-		type: "POST",
-		data :  form_data,
-		success: function(data){
-			allCompanies(data);
-		}
-	});
-}
-
