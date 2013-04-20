8e8818c39bc721b521fd40c3478da1d6cc37c52e
gshubham <shubhamgoel123@gmail.com>
2013-04-20 14:27:03 -0700

    Fixed parsing error

--- javascripts/companies.js
+++ javascripts/companies.js
@@ -7,7 +7,7 @@
 var companyData = new Array();
 
 function make_base_auth(user, password) {
-  var tok = user + ':' + pass;
+  var tok = user + ':' + password;
   var hash = Base64.encode(tok);
   return "Basic " + hash;
 }
